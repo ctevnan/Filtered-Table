370a117c1778af4282da2a5e3e45a6eab3d8c698
ctevnan <carolyntevnan@yahoo.com>
2016-04-13 22:23:31 -0400

    added filter

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
 angular.module('githubApp', ['ngTable'])
-.controller('GithubController', function($scope, $http, NgTableParams) {
+.controller('GithubController', function($scope, $http, $filter, NgTableParams) {
   $scope.githubTable = new NgTableParams({}, {
     getData: function($defer, params) {
       return $http.get('https://api.github.com/users/' + $scope.username + '/repos')
       .then(function (response) {
-        return response.data;
+        var sortedData = $filter('orderBy')(response.data, params.orderBy());
+        return sortedData;
       });  
     }
   });
