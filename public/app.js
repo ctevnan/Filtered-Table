5319766cfdd0e49cef45703f1c7f7ef5042524ab
ctevnan <carolyntevnan@yahoo.com>
2016-04-13 21:41:52 -0400

    making a commit because everything is working

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ angular.module('githubApp', ['ngTable'])
 .controller('GithubController', function($scope, $http, NgTableParams) {
   $scope.githubTable = new NgTableParams({}, {
     getData: function($defer, params) {
-      $http.get('https://api.github.com/users/' + $scope.username + '/repos')
+      return $http.get('https://api.github.com/users/' + $scope.username + '/repos')
       .then(function (response) {
         return response.data;
       });  
